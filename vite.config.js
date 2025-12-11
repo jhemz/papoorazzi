ff18ccb10f3f4af127605d6320864ddfd93b97a4
james <jamesfemarshall@googlemail.com>
2025-12-11 10:59:54 +0000

    Update Vite base for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/papoorazzi/',   // <-- add this line
+  base: '/',   // root of the domain
 })
